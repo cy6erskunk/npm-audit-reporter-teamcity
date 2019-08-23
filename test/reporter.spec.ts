366cfcb49a661d78b1e9d25ca7f5226de89b1e2c
Igor Shevchenko <cyberskunk@gmail.com>
2019-08-23 22:20:39 +0300

    Improve tests

--- test/reporter.spec.ts
+++ test/reporter.spec.ts
@@ -1,28 +1,28 @@
 const tsm = {
   inspection: jest.fn(),
   inspectionType: jest.fn(),
-}
+};
 
-import defaultConfig from '../src/config'
+import { defaultConfig } from '../src/config';
 import reporterFactory from '../src/reporter';
 
-import {multipleVulnerabilities, noVulnerabilities}  from './mocks/';
+import { multipleVulnerabilities, noVulnerabilities } from './mocks/';
 
-describe('nsp teamcity reporter', () => {
+describe('npm audit teamcity reporter', () => {
   beforeEach(() => {
-    tsm.inspectionType.mockReset()
-    tsm.inspection.mockReset()
-  })
-
-  test('no vulnerabilities found', () => {
-    reporterFactory(tsm, defaultConfig)(noVulnerabilities)
-    expect(tsm.inspectionType).not.toBeCalled()
-    expect(tsm.inspection).not.toBeCalled()
-  })
+    tsm.inspectionType.mockReset();
+    tsm.inspection.mockReset();
+  });
 
   test('a couple of vulnerabilities found', () => {
-    reporterFactory(tsm, defaultConfig)(multipleVulnerabilities)
-    expect(tsm.inspectionType.mock.calls.length).toBe(1)
-    expect(tsm.inspection.mock.calls.length).toBe(9)
-  })
-})
+    reporterFactory(tsm, defaultConfig)(multipleVulnerabilities);
+    expect(tsm.inspectionType).toHaveBeenCalledTimes(1);
+    expect(tsm.inspection).toHaveBeenCalledTimes(9);
+  });
+
+  test('no vulnerabilities found', () => {
+    reporterFactory(tsm, defaultConfig)(noVulnerabilities);
+    expect(tsm.inspectionType).not.toHaveBeenCalled();
+    expect(tsm.inspection).not.toHaveBeenCalled();
+  });
+});
