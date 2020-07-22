6fb2ce7d153fb47fabd6b5e14e8b68b5b8b763f5
Igor Shevchenko <cyberskunk@gmail.com>
2020-07-22 13:56:32 +0300

    Add `dependency of` field to output

--- src/reporter.ts
+++ src/reporter.ts
@@ -116,12 +116,24 @@ export default function reporter(
       Object.keys(auditResult.advisories).forEach((advisoryId) => {
         const advisoryElement = auditResult.advisories[advisoryId];
         debug('current element:', advisoryElement);
+
         tsm.inspection({
           SEVERITY: inspectionSeverity,
           file: `module: "${advisoryElement.module_name}"`,
           message: `${advisoryElement.overview}
 severity: ${advisoryElement.severity},
 versions: ${advisoryElement.findings.map((f) => f.version).join(', ')},
+dependency of: ${advisoryElement.findings
+            .reduce((acc, prev) => {
+              prev.paths.forEach((path) => {
+                const dependencyOf = path.split('>')[0];
+                if (!acc.includes(dependencyOf)) {
+                  acc.push(dependencyOf);
+                }
+              });
+              return acc;
+            }, [])
+            .join(', ')},
 vulnerable_versions: ${advisoryElement.vulnerable_versions},
 patched_versions: ${advisoryElement.patched_versions},
 recommendation: ${advisoryElement.recommendation},
